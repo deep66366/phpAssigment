89730060280b32d08b83aa7345add539fe101d30
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-11-02 19:17:23 +0530

    fixed 22

--- 22.Iterative_Functions.php
+++ 22.Iterative_Functions.php
@@ -1,12 +1,11 @@
 <?php
 echo "This is Original Array<br>";
 $a = array(4, 54, 6, 100, 0, -1, 400);
-foreach ($a as $value) {
+foreach ($a as $value)
     echo "$value, ";
-}
 echo "<br>The Current Element is :" . current($a);
 echo "<br>The Next Element is :" . next($a);
-echo "<br>The Previus Element is :" . prev($a);
-echo "<br>Reset By Reset() functions:" . reset($a);
+echo "<br>The Previous Element is :" . prev($a);
+echo "<br>Reset By Reset() function:" . reset($a);
 echo "<br>The End Element is :" . end($a);
 ?>
