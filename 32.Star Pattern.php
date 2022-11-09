cedf5cace51d667105dd46e6d5bd84802b96ae6e
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-11-09 20:00:27 +0530

    modified

--- 32.Star Pattern.php
+++ 32.Star Pattern.php
@@ -1,4 +1,12 @@
 <?php
+echo "Star Pattern 1<br>";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+echo "<br>Star Pattern 2<br>";
 for ($i = 0; $i < 3; $i++) {
     for ($j = 0; $j < (3 - $i); $j++) {
         echo "*";
