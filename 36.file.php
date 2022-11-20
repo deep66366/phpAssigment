c29d809880534b1a284108c4ae928b5121d9b8f6
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-11-20 21:16:14 +0530

    modified

--- 36.file.php
+++ 36.file.php
@@ -2,9 +2,14 @@
 
 // Create a new file
 $my_file = 'file.txt';
-$handle = fopen($my_file, 'w') or die('Cannot open file:  ' . $my_file);
-$data = 'This is the data';
+$handle = fopen($my_file, 'w');
+if (!$handle) {
+    die('Cannot open file:  ' . $my_file);
+}
+echo "File open Successfully";
+
 //Write into a file
+$data = 'This is the data';
 fwrite($handle, $data);
 
 // Read the contents of the file
